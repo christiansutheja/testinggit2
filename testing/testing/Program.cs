f1a7bcd1873819a25781cfd432ba1f968b85818b
christiansutheja <christian.sutheja@mitrais.com>
2015-08-18 18:21:06 +0800

    test

--- testing/testing/Program.cs
+++ testing/testing/Program.cs
@@ -10,7 +10,7 @@ namespace testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("console writeline!!!!!!!!!!!!!!!!!!!!");
         }
     }
 }
